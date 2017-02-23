9f44dbf9d1cf98bf2bdb698f44c60d93c2eb28c1
RASG <yodog@users.noreply.github.com>
2017-02-23 10:14:16 -0300

    Update JQ_ForceAutoComplete.user.js

--- JQ_ForceAutoComplete.user.js
+++ JQ_ForceAutoComplete.user.js
@@ -2,18 +2,40 @@
 // @name            JQ_ForceAutoComplete
 // @namespace       http://stackoverflow.com/users/982924/rasg
 // @author          RASG
-// @version         2012.09.21
+// @version         2017.02.23
 // @description     Forces the autocomplete attribute for all forms and input fields in the page
 // @require         http://code.jquery.com/jquery.min.js
 // @include         http*://*
-// @grant           GM_log
 // ==/UserScript==
-
 
 $(window).load(function(){
 
-    $("body")
-        .on("focus click", "form", function(){ $(this).removeAttr("disabled readonly").attr("autocomplete", "on") })
-        .on("focus click", "input", function(){ $(this).removeAttr("disabled readonly").attr("autocomplete", "on") })
+    // ---
+    // SELECT ELEMENTS TO WORK ON
+    // ---
+
+    $("body").on("focus click", "form" , function() { parse( this ) });
+    $("body").on("focus click", "input", function() { parse( this ) });
+
+    // ---
+    // FUNCTION TO ADD / REMOVE ATTRIBUTES FROM THE ELEMENTS
+    // ---
+
+    function parse(element) {
+
+        //console.log(element);
+
+        $(element).removeAttr("disabled readonly");
+        $(element).removeProp("disabled readonly");
+
+        $(element).attr("autocomplete", "on");
+        $(element).prop("autocomplete", "on");
+    }
+
+    // ---
+    // KNOWN FUNCTIONS THAT PREVENTS AUTOCOMPLETE FROM WORKING
+    // ---
+
+    unsafeWindow.C = function(G) { return false };
 
 });
