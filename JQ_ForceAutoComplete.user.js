a31f426ad1e0ee7c25249b6ea3122835d4d2efd2
RASG <yodog@users.noreply.github.com>
2017-12-12 16:33:24 -0200

    Update JQ_ForceAutoComplete.user.js

--- JQ_ForceAutoComplete.user.js
+++ JQ_ForceAutoComplete.user.js
@@ -2,24 +2,63 @@
 // @name            JQ_ForceAutoComplete
 // @namespace       http://stackoverflow.com/users/982924/rasg
 // @author          RASG
-// @version         2017.04.15
+// @version         2017.12.12
 // @description     Forces the autocomplete attribute for all forms and input fields in the page
 // @require         http://code.jquery.com/jquery.min.js
+// @require         https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
 // @include         http*://*
+// @grant           GM_getValue
+// @grant           GM_setValue
+// @grant           GM_addStyle
+// @grant           GM_getMetadata
+// @grant           GM_registerMenuCommand
 // ==/UserScript==
 
-if (window.$ !== undefined) { $ = window.$ }
-else if (unsafeWindow.$ !== undefined) { $ = unsafeWindow.$ }
-else { this.$ = this.jQuery = jQuery.noConflict(true); }
+// PREVENT JQUERY CONFLICT
+this.$ = this.jQuery = jQuery.noConflict(true);
 
 $(window).load(function(){
+
+    // ---
+    // PARSE PASSWORD FIELDS
+    // ---
+
+    password_fields = $("input:password");
+
+    function togglePassword() {
+        show_password_as_clear_text ? $(password_fields).attr("type", "text") : $(password_fields).attr("type", "password");
+    };
+
+    // ---
+    // OPTIONS / CONFIG MENU
+    // ---
+
+    var cfg = new MonkeyConfig({
+        title: 'Config JQ_ForceAutoComplete',
+        menuCommand: true,
+        onSave: function() { togglePassword(); },
+        params: {
+            save_password: {
+                type: 'checkbox',
+                default: true
+            },
+            enable_field: {
+                type: 'checkbox',
+                default: true
+            },
+            show_password_as_clear_text: {
+                type: 'checkbox',
+                default: false
+            }
+        }
+    });
 
     // ---
     // SELECT ELEMENTS TO WORK ON
     // ---
 
-    $("body").on("focus click", "form" , function() { parse( this ) });
-    $("body").on("focus click", "input", function() { parse( this ) });
+    $("body").on("focus click focusout", "form" , function() { parse( this ) });
+    $("body").on("focus click focusout", "input", function() { parse( this ) });
 
     // ---
     // FUNCTION TO ADD / REMOVE ATTRIBUTES FROM THE ELEMENTS
@@ -27,13 +66,21 @@ $(window).load(function(){
 
     function parse(element) {
 
-        //console.log(element);
+        enable_field                = cfg.get("enable_field");
+        save_password               = cfg.get("save_password");
+        show_password_as_clear_text = cfg.get("show_password_as_clear_text");
 
-        $(element).removeAttr("disabled readonly");
-        $(element).removeProp("disabled readonly");
+        togglePassword();
 
-        $(element).attr("autocomplete", "on");
-        $(element).prop("autocomplete", "on");
+        if ( enable_field ) {
+            $(element).removeAttr("disabled readonly");
+            $(element).removeProp("disabled readonly");
+        }
+
+        if ( save_password ) {
+            $(element).attr("autocomplete", "on");
+            $(element).prop("autocomplete", "on");
+        }
     }
 
     // ---
