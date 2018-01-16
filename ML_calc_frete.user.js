182aa7a0230da8da16dbc061d1a691d9a4d7aea6
RASG <yodog@users.noreply.github.com>
2018-01-16 17:51:20 -0200

    Update ML_calc_frete.user.js

--- ML_calc_frete.user.js
+++ ML_calc_frete.user.js
@@ -2,39 +2,69 @@
 // @name        Mercado Livre - calculadora de frete
 // @namespace   http://stackoverflow.com/users/982924/rasg
 // @author      RASG
-// @description Mercado Livre - calculadora de frete
+// @description Adiciona campo somando o valor do frete na visualizacao em lista do Mercado Livre
 // @require     http://code.jquery.com/jquery.min.js
+// @require     https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
 // @include     http*://*.mercadolivre.com.br/*
-// @version     2018.01.16.1642
+// @version     2018.01.16.1751
+// @grant       GM_addStyle
+// @grant       GM_getMetadata
+// @grant       GM_getValue
+// @grant       GM_registerMenuCommand
+// @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
 
+// PREVENT JQUERY CONFLICT
+this.$ = this.jQuery = jQuery.noConflict(true);
+
 $(window).load(function(){
+
+    // ---
+    // OPTIONS / CONFIG MENU
+    // ---
+
+    var cfg = new MonkeyConfig({
+        title: 'Config ML_calc_frete',
+        menuCommand: true,
+        onSave: function() { recarregar(); },
+        params: {
+            esconder_frete_maior_que: {
+                type: 'number',
+                default: 99
+            },
+            esconder_total_maior_que: {
+                type: 'number',
+                default: 999
+            }
+        }
+    });
+
+    var esconder_frete_maior_que = cfg.get("esconder_frete_maior_que");
+    var esconder_total_maior_que = cfg.get("esconder_total_maior_que");
+
+    // ---
+    // ELEMENTS
+    // ---
 
     //$('.item__info:lt(2)').each(function() {
     //$('div.rowItem:lt(2)').each(function() {
     $('div.rowItem').each(function() {
 
-        iteminfotitle = $(this).find('.item__info-title');
-        link          = iteminfotitle.attr('href');
+        var rowItem = $(this);
 
-        //console.log( 'link: ', link );
+        var id = rowItem.attr('id');
 
-        var id = $(this).attr('id');
+        var iteminfotitle = rowItem.find('.item__info-title');
+        link              = iteminfotitle.attr('href');
 
-        //console.log( 'id: ', id );
-
-        var itemprice = $(this).find('.item__price');
+        var itemprice = rowItem.find('.item__price');
         itemprice.append('<span class="freteholder"> Envio: <span id="freteholder_'+id+'">?</span></span> <span class="totalholder"> Total: R$ <span id="totalholder_'+id+'">?</span></span>');
         $('.freteholder').css('color', 'blue');
         $('.totalholder').css('color', 'red');
 
-        //console.log( 'itemprice: ', itemprice );
-
-        pricefraction = $(this).find('.price-fraction');
+        pricefraction = rowItem.find('.price-fraction');
         var preco     = pricefraction.text();
-
-        //console.log( 'preco: ', preco );
 
         conectar('GET', link, resparser);
 
@@ -52,29 +82,29 @@ $(window).load(function(){
         }
 
         function resparser(detalhes) {
-            //console.log( 'detalhes: ', detalhes );
-            //console.log( 'detalhes.responseText: ', detalhes.responseText );
-            //console.log( 'detalhes.response: ', detalhes.response );
 
             elfrete = $(detalhes.responseText).find('.shipping-method-title > .ch-price').contents().filter(function() { return this.nodeType == 3; });
             valorfrete = elfrete.text();
 
-            n1 = valorfrete.replace(/\D/g,'');
-            n2 = preco.replace(/\D/g,'');
+            n1 = parseInt( valorfrete.replace(/\D/g,'') );
+            n2 = parseInt( preco.replace(/\D/g,'') );
 
-            valortotal = parseInt(n1) + parseInt(n2);
-
-            //console.log( 'elfrete: ', elfrete );
-            //console.log( 'valorfrete: ', valorfrete );
-            //console.log( 'n1: ', n1 );
-            //console.log( 'n2: ', n2 );
-            //console.log( 'valortotal: ', valortotal );
-            //console.log( 'itemprice2: ', itemprice );
-            //console.log( 'preco2: ', preco );
-            //console.log( 'id2: ', id );
+            valortotal = n1 + n2;
 
             itemprice.find('#freteholder_'+id).html(valorfrete);
             itemprice.find('#totalholder_'+id).html(valortotal);
+
+            if ( esconder_frete_maior_que ) {
+                if ( n1 > esconder_frete_maior_que ) rowItem.parent().hide();
+            }
+
+            if ( esconder_total_maior_que ) {
+                if ( valortotal > esconder_total_maior_que ) rowItem.parent().hide();
+            }
         }
     })
 });
+
+function recarregar() {
+    alert('Recarregue a pagina para aplicar as alteracoes');
+}
