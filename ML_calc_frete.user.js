588ecace49f8ec4a56529b38565a2abbf44a089e
RASG <yodog@users.noreply.github.com>
2018-01-19 13:21:09 -0200

    Update ML_calc_frete.user.js

--- ML_calc_frete.user.js
+++ ML_calc_frete.user.js
@@ -6,7 +6,7 @@
 // @require     http://code.jquery.com/jquery.min.js
 // @require     https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
 // @include     http*://*.mercadolivre.com.br/*
-// @version     2018.01.18.2042
+// @version     2018.01.19.1321
 // @grant       GM_addStyle
 // @grant       GM_getMetadata
 // @grant       GM_getValue
@@ -42,6 +42,10 @@ $(window).load(function(){
                 type: 'checkbox',
                 default: false
             },
+            expandir_area_de_visualizacao: {
+                type: 'checkbox',
+                default: false
+            },
             esconder_frete_maior_que: {
                 type: 'number',
                 default: 99
@@ -53,32 +57,34 @@ $(window).load(function(){
         }
     });
 
-    var ordenar_por_total         = cfg.get("ordenar_por_total");
-    var destacar_frete_gratis     = cfg.get("destacar_frete_gratis");
-    var esconder_frete_a_combinar = cfg.get("esconder_frete_a_combinar");
-    var esconder_frete_maior_que  = cfg.get("esconder_frete_maior_que");
-    var esconder_total_maior_que  = cfg.get("esconder_total_maior_que");
+    var ordenar_por_total             = cfg.get("ordenar_por_total");
+    var destacar_frete_gratis         = cfg.get("destacar_frete_gratis");
+    var esconder_frete_a_combinar     = cfg.get("esconder_frete_a_combinar");
+    var expandir_area_de_visualizacao = cfg.get("expandir_area_de_visualizacao");
+    var esconder_frete_maior_que      = cfg.get("esconder_frete_maior_que");
+    var esconder_total_maior_que      = cfg.get("esconder_total_maior_que");
 
     // ---
     // ELEMENTS
     // ---
 
-    var rowItems = $('div.rowItem');
+    var lista = $('ol#searchResults');
+    var items = lista.find('li.results-item');
 
-    rowItems.each(function() {
+    if ( expandir_area_de_visualizacao ) $('.ml-main').attr('style', 'max-width: 1700px !important');
 
-        var rowItem = $(this);
+    items.each(function() {
 
-        var id = rowItem.attr('id');
+        var item = $(this);
+        var id   = item.find('div.rowItem').attr('id');
 
         // capturar o link
 
-        var iteminfotitle = rowItem.find('.item__info-title');
-        link = iteminfotitle.attr('href');
+        var link = item.find('a.item__info-title').attr('href') || item.find('a.item-link, a.item__info-link').attr('href');
 
         // adicionar o elemento que ira receber os meus campos 'envio' e 'total'
 
-        var itemprice = rowItem.find('.item__price');
+        var itemprice = item.find('div.item__price');
         itemprice.append('<span class="freteholder"> Envio: R$ <span id="freteholder_'+id+'">?</span></span> <span class="totalholder"> Total: R$ <span id="totalholder_'+id+'" class="totals">?</span></span>');
         $('.totalholder').css('color', 'red');
 
@@ -87,7 +93,7 @@ $(window).load(function(){
 
         // capturar o preco
 
-        pricefraction = rowItem.find('.price-fraction');
+        var pricefraction = item.find('span.price__fraction');
         var preco = pricefraction.text();
         preco = parseInt( preco.replace(/\D/g,'') );
 
@@ -97,10 +103,9 @@ $(window).load(function(){
 
         // capturar o texto; se for frete gratis marcar com borda verde e nao enviar a requisicao
 
-        var stackiteminfo = rowItem.find('.stack-item-info ');
-        regrafrete = stackiteminfo.text();
+        var regrafrete = item.find('p.stack-item-info ').text() || item.find('div.item__shipping ').attr('title');
 
-        if ( regrafrete.indexOf('Frete gr') > 0 ) {
+        if ( (regrafrete) && (regrafrete.indexOf('Frete gr') > -1) ) {
             color      = 'green';
             valorfrete = 0;
             valortotal = preco + valorfrete;
@@ -108,13 +113,13 @@ $(window).load(function(){
             freteholder_id.html(valorfrete);
             totalholder_id.html(valortotal);
 
-            if ( destacar_frete_gratis ) rowItem.parent().css('border', '2px dotted ' + color);
+            if ( destacar_frete_gratis ) item.css('border', '2px dotted ' + color);
 
             if ( valortotal > esconder_total_maior_que ) {
-                rowItem.parent().hide();
+                item.hide();
             }
             else {
-                if ( ordenar_por_total ) sortUsingNestedText($('#searchResults'), "li", "span.totals");
+                if ( ordenar_por_total ) sortUsingNestedText(lista, items, "span.totals");
             }
         }
         else {
@@ -157,7 +162,7 @@ $(window).load(function(){
                 itemprice.find('.freteholder').hide();
                 itemprice.find('.totalholder').html(textofrete);
 
-                if ( esconder_frete_a_combinar ) rowItem.parent().hide();
+                if ( esconder_frete_a_combinar ) item.hide();
             }
             else {
                 valortotal = preco + valorfrete;
@@ -166,10 +171,10 @@ $(window).load(function(){
                 totalholder_id.html(valortotal);
 
                 if ( (valorfrete > esconder_frete_maior_que) || (valortotal > esconder_total_maior_que) ) {
-                    rowItem.parent().hide();
+                    item.hide();
                 }
                 else {
-                    if ( ordenar_por_total ) sortUsingNestedText($('#searchResults'), "li", "span.totals");
+                    if ( ordenar_por_total ) sortUsingNestedText(lista, items, "span.totals");
                 }
             }
         }
