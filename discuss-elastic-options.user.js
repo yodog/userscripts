d100818c72479c442076800b95ae36ed8ca561c9
RASG <yodog@users.noreply.github.com>
2019-12-27 17:50:15 -0300

    Update discuss-elastic-options.user.js

--- discuss-elastic-options.user.js
+++ discuss-elastic-options.user.js
@@ -6,7 +6,7 @@
 // @require     http://code.jquery.com/jquery.min.js
 // @require     https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
 // @include     http*://*discuss.elastic.co/*
-// @version     2019.12.27.1446
+// @version     2019.12.27.1750
 // @grant       GM_addStyle
 // @grant       GM_getMetadata
 // @grant       GM_getValue
@@ -15,40 +15,55 @@
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
 
+// -----------------------------------------------------------------------------
 // PREVENT JQUERY CONFLICT
+// -----------------------------------------------------------------------------
+
+var $      = window.$;
+var jQuery = window.jQuery;
+
 this.$ = this.jQuery = jQuery.noConflict(true);
 
+if (typeof $ == 'undefined') console.log('JQuery not found; The script will certainly fail');
+
+// -----------------------------------------------------------------------------
+// OPTIONS / CONFIG MENU
+// -----------------------------------------------------------------------------
+
+var parametros = {
+    code_box_expand  : { type: 'checkbox', default: true },
+    code_box_height  : { type: 'number',   default: 900 },
+    code_font_resize : { type: 'checkbox', default: true },
+    code_font_size   : { type: 'number',   default: 12 },
+    page_wide        : { type: 'checkbox', default: true },
+};
+
+var cfg;
+try {
+    cfg = new MonkeyConfig({
+        title:       'Config DE_code_box',
+        menuCommand: true,
+        onSave:      function() { recarregar(); },
+        params:      parametros
+    });
+    console.log("MonkeyConfig loaded; The settings menu will be enabled");
+}
+catch(err) {
+    console.log(err);
+    console.log("MonkeyConfig not loaded; The settings menu will be disabled");
+    cfg = {
+        params: parametros,
+        get:    function get(name) { return GM_getValue(name, this.params[name].default) }
+    }
+}
+
+// -----------------------------------------------------------------------------
 // START
+// -----------------------------------------------------------------------------
+
+var shouldreload = false;
+
 $(function(){
-
-    // ---
-    // OPTIONS / CONFIG MENU
-    // ---
-
-    try {
-        var cfg = new MonkeyConfig({
-            title: 'Config DE_code_box',
-            menuCommand: true,
-            onSave: function() { recarregar(); },
-            params: {
-                code_box_expand  : { type: 'checkbox', default: true },
-                code_box_height  : { type: 'number',   default: 900 },
-                code_font_resize : { type: 'checkbox', default: true },
-                code_font_size   : { type: 'number',   default: 12 },
-                page_wide        : { type: 'checkbox', default: true },
-            }
-        });
-        console.log("Created var cfg = new MonkeyConfig");
-    }
-    catch(err) {
-        console.log("Could not create var cfg = new MonkeyConfig");
-    }
-
-    var code_box_expand  = (!!cfg) ? cfg.get("code_box_expand")  : true;
-    var code_box_height  = (!!cfg) ? cfg.get("code_box_height")  : 900;
-    var code_font_resize = (!!cfg) ? cfg.get("code_font_resize") : true;
-    var code_font_size   = (!!cfg) ? cfg.get("code_font_size")   : 12;
-    var page_wide        = (!!cfg) ? cfg.get("page_wide")        : true;
 
     // ---
     // MODS
@@ -56,12 +71,18 @@ $(function(){
 
     $(document).on('ready scroll', function() {
 
-        if ( code_box_expand ) $('code').css({'max-height':code_box_height + 'px'})
+        if ( cfg.get("code_box_expand") ) {
+            var code_box_height = cfg.get("code_box_height");
+            $('code').css({'max-height':code_box_height + 'px'});
+            shouldreload = true;
+        }
 
-        if ( code_font_resize ) $('blockquote, code, pre').css({'font-size':code_font_size + 'px'})
+        if ( cfg.get("code_font_resize") ) {
+            var code_font_size = cfg.get("code_font_size");
+            $('blockquote, code, pre').css({'font-size':code_font_size + 'px'});
+        }
 
-        if ( page_wide ) {
-
+        if ( cfg.get("page_wide") ) {
             $('#main-outlet').css({'max-width':'none'});
             $('.timeline-container').css({'margin-left':'90%'});
             $('.topic-body').css({
@@ -69,6 +90,7 @@ $(function(){
                 'float':'unset',
                 'width':'unset'
             });
+            shouldreload = true;
         }
     });
 });
@@ -76,6 +98,7 @@ $(function(){
 // -----------------------------------------------------------------------------
 
 function recarregar() {
+    if (! shouldreload) return;
     //alert('Recarregue a pagina para aplicar as alteracoes');
     document.location.reload(false);
 }
