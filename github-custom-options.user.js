d52921554e95baceff158e59ca8b0d0efc7e4fe9
RASG <yodog@users.noreply.github.com>
2020-01-16 16:51:13 -0300

    // @version     2020.01.16.1650

--- github-custom-options.user.js
+++ github-custom-options.user.js
@@ -7,9 +7,9 @@
 // @require     https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
 // @require     https://cdn.jsdelivr.net/npm/siiimple-toast/dist/siiimple-toast.min.js
 // @resource    toastcss  https://cdn.jsdelivr.net/npm/siiimple-toast/dist/style.css
-// @include     https://github.com/*
+// @include     http*://github.com/*
 // @icon        https://www.google.com/s2/favicons?domain=github.com
-// @version     2020.01.16.1027
+// @version     2020.01.16.1650
 // @grant       GM_addStyle
 // @grant       GM_getMetadata
 // @grant       GM_getResourceText
@@ -18,6 +18,8 @@
 // @grant       GM_registerMenuCommand
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
+// @run-at      document-start
+// @noframes
 // ==/UserScript==
 
 // -----------------------------------------------------------------------------
@@ -33,7 +35,7 @@ GM_addStyle( GM_getResourceText("toastcss") );
 
 var toast = siiimpleToast.setOptions({
     position: 'top|right',
-    duration: 3000,
+    duration: 4000,
 });
 
 // -----------------------------------------------------------------------------
@@ -52,7 +54,7 @@ if (typeof $ == 'undefined') console.log('JQuery not found; The script will cert
 // -----------------------------------------------------------------------------
 
 var parametros = {
-    code_font_resize : { type: 'checkbox', default: true },
+    code_font_resize : { type: 'checkbox', default: false },
     code_font_size   : { type: 'number',   default: 12 },
     page_wide        : { type: 'checkbox', default: true },
 };
@@ -62,7 +64,7 @@ try {
     cfg = new MonkeyConfig({
         title:       'Config Github Options',
         menuCommand: true,
-        onSave:      function() { recarregar(); },
+        onSave:      function() { fnSaveChanges(); },
         params:      parametros
     });
     console.log("MonkeyConfig loaded; The settings menu will be enabled");
@@ -82,23 +84,38 @@ catch(err) {
 
 var shouldreload = false;
 
-$(document).on('ready scroll', function() {
+// apply imediately at document start
+fnCheckChanges();
 
-    if ( cfg.get("code_font_resize") ) {
-        var code_font_size = cfg.get("code_font_size");
-        $('td.blob-code-inner, td.blob-num').css({'font-size':code_font_size + 'px'});
-    }
+// also wait for page load. jquery will be ready here
+$(function() {
 
-    if ( cfg.get("page_wide") ) {
-        $('div.container-lg').css({'max-width':'unset'});
-        shouldreload = true;
-    }
+    // monitor the page for changes and reapply if necessary
+    // use 'observer.disconnect()' in 'fnCheckChanges()' to stop monitoring
+    var alvo = document.querySelector('body');
+    var observer = new MutationObserver(fnCheckChanges);
+    observer.observe(alvo, { attributes: false, characterData: false, childList: true, subtree: true });
 
 });
 
 // -----------------------------------------------------------------------------
+// FUNCTIONS
+// -----------------------------------------------------------------------------
 
-function recarregar() {
+function fnCheckChanges(changes, observer) {
+
+    var code_font_size = '';
+    if (cfg.get("code_font_resize")) code_font_size = cfg.get("code_font_size") + 'px';
+    $('td.blob-code-inner, td.blob-num').css({'font-size':code_font_size});
+
+    var page_size = '';
+    if (cfg.get("page_wide")) page_size = 'unset';
+    $('div.container-lg').css({'max-width':page_size});
+}
+
+// -----------------------------------------------------------------------------
+
+function fnSaveChanges() {
 
     $('body').on("click", "#reloadnow", function() {
         document.location.reload(false);
@@ -108,5 +125,5 @@ function recarregar() {
     toast.success(msg_success);
 
     var msg_reload = '<span id="reloadnow"> Some changes will be applied after you reload the page. <br> Click here to reload now </span>';
-    if (shouldreload) toast.message(msg_reload, { delay:2000, duration:5000 });
+    if (shouldreload) toast.message(msg_reload, { delay: 3000, duration: 7000 });
 }
