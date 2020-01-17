b13623090ef9ecb13b9aac13a90527cf73885d7c
RASG <yodog@users.noreply.github.com>
2020-01-17 13:38:25 -0300

    // @version        2012.01.01.1010

--- troca-http-por-https.user.js
+++ troca-http-por-https.user.js
@@ -4,7 +4,7 @@
 // @author         RASG
 // @description    [PT] Troca o protocolo 'http' por 'https' (seguro) para TODOS os sites (se este nao tiver uma versao segura, causa erro)
 // @include        http://*
-// @version        2012.01.01
+// @version        2012.01.01.1010
 // ==/UserScript==
 
 (function() {
