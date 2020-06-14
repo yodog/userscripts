f2d2718531003cd88fdc82f52bdf0245907e0258
RASG <yodog@users.noreply.github.com>
2020-06-13 23:58:44 -0300

    // @version      2020.06.13.2358

--- gitlab-kpi.user.js
+++ gitlab-kpi.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitLab Metrics
 // @namespace    http://stackoverflow.com/users/982924/rasg
-// @version      2020.06.13.1400
+// @version      2020.06.13.2358
 // @description  KPI
 // @author       RASG
 // @match        http*://git.serpro/*
@@ -51,28 +51,28 @@ if (typeof $ == 'undefined') console.log('JQuery not found; The script will cert
 
 (function() {
 
-    $('#nav-groups-dropdown').clone().prop('id', 'btnKPI').appendTo('ul.navbar-sub-nav');
-    $('#btnKPI > button.btn').text('KPI');
-    //$('#btnKPI .frequent-items-dropdown-container').empty();
-
     var menu_mes = `
-<ul>
-<li id="jan" data-start="2020-01-01" data-end="2020-01-30"> <a> Janeiro   </a> </li>
-<li id="fev" data-start="2020-02-01" data-end="2020-02-29"> <a> Fevereiro </a> </li>
-<li id="mar" data-start="2020-03-01" data-end="2020-03-31"> <a> Março     </a> </li>
-<li id="abr" data-start="2020-04-01" data-end="2020-04-30"> <a> Abril     </a> </li>
-<li id="mai" data-start="2020-05-01" data-end="2020-05-31"> <a> Maio      </a> </li>
-<li id="jun" data-start="2020-06-01" data-end="2020-06-30"> <a> Junho     </a> </li>
-<li id="jul" data-start="2020-07-01" data-end="2020-07-30"> <a> Julho     </a> </li>
-<li id="ago" data-start="2020-08-01" data-end="2020-08-31"> <a> Agosto    </a> </li>
-<li id="set" data-start="2020-09-01" data-end="2020-09-30"> <a> Setembro  </a> </li>
-<li id="out" data-start="2020-10-01" data-end="2020-10-31"> <a> Outubro   </a> </li>
-<li id="nov" data-start="2020-11-01" data-end="2020-11-30"> <a> Novembro  </a> </li>
-<li id="dez" data-start="2020-12-01" data-end="2020-12-31"> <a> Dezembro  </a> </li>
+<ul id="meses">
+<li id="1"  tabindex="1"  data-start="2020-01-01" data-end="2020-01-30"> <a> Janeiro   </a> </li>
+<li id="2"  tabindex="2"  data-start="2020-02-01" data-end="2020-02-29"> <a> Fevereiro </a> </li>
+<li id="3"  tabindex="3"  data-start="2020-03-01" data-end="2020-03-31"> <a> Março     </a> </li>
+<li id="4"  tabindex="4"  data-start="2020-04-01" data-end="2020-04-30"> <a> Abril     </a> </li>
+<li id="5"  tabindex="5"  data-start="2020-05-01" data-end="2020-05-31"> <a> Maio      </a> </li>
+<li id="6"  tabindex="6"  data-start="2020-06-01" data-end="2020-06-30"> <a> Junho     </a> </li>
+<li id="7"  tabindex="7"  data-start="2020-07-01" data-end="2020-07-30"> <a> Julho     </a> </li>
+<li id="8"  tabindex="8"  data-start="2020-08-01" data-end="2020-08-31"> <a> Agosto    </a> </li>
+<li id="9"  tabindex="9"  data-start="2020-09-01" data-end="2020-09-30"> <a> Setembro  </a> </li>
+<li id="10" tabindex="10" data-start="2020-10-01" data-end="2020-10-31"> <a> Outubro   </a> </li>
+<li id="11" tabindex="11" data-start="2020-11-01" data-end="2020-11-30"> <a> Novembro  </a> </li>
+<li id="12" tabindex="12" data-start="2020-12-01" data-end="2020-12-31"> <a> Dezembro  </a> </li>
 </ul>
 `
 
 var styles = `
+#meses li:focus {
+background-color: #eee;
+}
+
 .gridjs {
 font-size: 1em;
 }
@@ -101,24 +101,41 @@ max-width: unset;
 width: unset;
 }
 `
-
 var styleSheet = document.createElement("style")
 styleSheet.type = "text/css"
 styleSheet.innerText = styles
 document.head.appendChild(styleSheet)
 
-
+    $('#nav-groups-dropdown').clone().prop('id', 'btnKPI').appendTo('ul.navbar-sub-nav');
+    $('#btnKPI > button.btn').text('KPI');
+    $('#btnKPI').click( () => $(`#meses #${mes}`).click() );
+    //$('#btnKPI .frequent-items-dropdown-container').empty();
 
     const meu_cpf = $('a.header-user-dropdown-toggle').attr('href').replace(/\D/g, '');
     //var url = `https://git.serpro/api/v4/projects/8969/issues?assignee_username=${meu_cpf}&updated_after=2020-06-01&updated_before=2020-07-01&per_page=100`;
     const project_issues_url = (start, end) => `https://git.serpro/api/v4/projects/8969/issues?updated_after=${start}&updated_before=${end}&per_page=100`;
+
+    const date      = new Date();
+    const ano       = date.getYear() + 1900;
+    const mes       = date.getMonth() + 1;
+    const dia       = date.getDate();
+    const diasuteis = (function diasuteis() {
+        var du = dia;
+        for (var d=0; d<dia; d++) {
+            var t = new Date(ano, mes-1, d);
+            if (t.getDay(d) == 0 || t.getDay(d) == 6) du--;
+        }
+        return du;
+    }());
+
+    console.log('dia', dia);
+    console.log('diasuteis', diasuteis);
 
     $('#btnKPI .frequent-items-dropdown-sidebar').prop('id', 'sidebarKPI').html(menu_mes);
     $('#btnKPI .frequent-items-dropdown-content').prop('id', 'contentKPI').css({'font-size':'12px'}).empty();
     $('#btnKPI .dropdown-menu li a').css({'padding-top':'5px'});
 
     $('#sidebarKPI li').click(function(e) {
-        //console.log();
         getJsonPages( project_issues_url( $(this).data('start'), $(this).data('end') ));
         return false;
     });
@@ -151,7 +168,7 @@ document.head.appendChild(styleSheet)
             }
             else {
                 dadosTratados = tratarDados(dadosFiltrados);
-                montarTabela(['Nome', 'Issues', 'Horas', 'Media'], dadosTratados, 'contentKPI');
+                montarTabela(['Nome', 'Issues', 'Horas', 'Media 1', 'Media 2'], dadosTratados, 'contentKPI');
             }
         });
     }(project_issues_url);
@@ -191,15 +208,16 @@ document.head.appendChild(styleSheet)
     // ---
 
     function tratarDados(dados) {
-        r = [];
-        var nome, issues, segundos, horas;
+        const r = [];
+        var nome, issues, segundos, horas, media_d, media_i;
         for (var k in dados) {
             nome     = dados[k][0].name.split(' ')[0];
             issues   = dados[k].length;
             segundos = dados[k].map(o => o.total_time_spent).reduce((a, c) => { return a + c });
             horas    = Math.floor(segundos / 60 / 60);
-            media    = (horas / issues).toFixed(1);
-            r.push([nome, issues, horas, media]);
+            media_d  = (horas / diasuteis).toFixed(1);
+            media_i  = (horas / issues).toFixed(1);
+            r.push([nome, issues, horas, media_d, media_i]);
         };
         console.log(dados)
         return r;
@@ -213,7 +231,7 @@ document.head.appendChild(styleSheet)
         //$(`#${elementID}`).empty();
         const grid = new gridjs.Grid({
             columns: columns,
-            search: true,
+            //search: true,
             data: data.sort(),
         }).render(document.getElementById(elementID));
     }
